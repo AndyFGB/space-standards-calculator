a311dafd47277e4bbda9ae8012c88d40906a03c5
AndyFGB <90258870+AndyFGB@users.noreply.github.com>
2025-02-24 13:57:04 +0000

    Dark Mode Commit 3

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '../components/ThemeProvider'
+import { ThemeProvider } from '@/components/ThemeProvider'
 import './globals.css'
 
 export default function RootLayout({ children }) {
